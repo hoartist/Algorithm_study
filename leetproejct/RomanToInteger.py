21ef9a582bc25f6d3b527fc2a335485b47480914
hoartist <69453448+hoartist@users.noreply.github.com>
2022-07-04 14:07:36 +0900

    Add files via upload

---
+++ leetproejct/RomanToInteger.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self,s:str)->int:
+        roman2Value={'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,
+                     'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+        value=0
+        temp=''
+        cursor=0
+        while cursor<len(s):
+            if(cursor+1)!=len(s) and s[cursor]+s[cursor+1] in roman2Value:
+                value=roman2Value[s[cursor]+s[cursor+1]]
+                cursor+=2
+            else:
+                value+=roman2Value[s[cursor]]
+                cursor+=1
+                return value
